567243c1c847c615cc033080eab728cc9220d10b
BUGmakerrr <799745201@qq.com>
2020-08-01 00:46:27 +0800

    3rd commit

--- test.cpp
+++ test.cpp
@@ -2,23 +2,43 @@
 
 using namespace std;
 
-int find(int* nums, int numsSize){
-	for (int i = 0; i < numsSize; ++i){
-		for (int j = 0; j < numsSize; ++j){
-			if (nums[i] = nums[j] || i != j) {
-				return nums[i];				
+int findRepeatNumber(int* nums, int numsSize){
+	int i = 0;
+	int j = numsSize - 1;
+	while (i <= j)
+	{
+		int mid = (i + j) / 2;
+		if (nums[mid] == mid)
+		{
+			if (mid != numsSize - 1)
+			{
+				i = mid + 1;
+			}
+			else
+			{
+				return mid + 1;
 			}
 		}
+		else if (mid == 0)
+		{
+			return 0;
+		}
+		else if (nums[mid - 1] == mid - 1 && mid > 0)
+		{
+			return mid;
+			
+		}
+		else
+		{
+			j = mid - 1;
+		}
+
 	}
 }
+
 int main(){
-	int T_nums[10];
-	int a = 0;
-	for (a = 0; a < 10; ++a)
-	{
-		cin >> T_nums[a];
-	}
-	cout << find(T_nums, 10) << endl;
+
+
 	getchar();
 	return 0;
 }
